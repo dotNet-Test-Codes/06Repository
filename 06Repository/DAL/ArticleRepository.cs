b957664ff64c80ef27ef6ad0f3b34adbd8755315
Rashmika9712 <rashmikamadushansamarakoon@gmail.com>
2023-04-17 18:07:11 +0530

    implment repo

---
+++ 06Repository/DAL/ArticleRepository.cs
@@ -0,0 +1,63 @@
+﻿using _06Repository.Data;
+using _06Repository.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace _06Repository.DAL
+{
+    public class ArticleRepository : IArticleRepository, IDisposable
+    {
+        private ArticleDBContext _context;
+        public ArticleRepository(ArticleDBContext context)
+        {
+            _context = context;
+        }
+        public void Create(Article article)
+        {
+            _context.Articles.Add(article);
+        }
+
+        public void Delete(int ArticleId)
+        {
+            Article article = _context.Articles.Find(ArticleId);
+            _context.Articles.Remove(article);
+        }
+
+        public Article Get(int ArticleId)
+        {
+            return _context.Articles.Find(ArticleId);
+        }
+
+        public IEnumerable<Article> GetAll()
+        {
+            return _context.Articles.ToList();
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Update(Article article)
+        {
+            _context.Entry(article).State = EntityState.Modified;
+        }
+
+        private bool disposed = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ 06Repository/DAL/IArticleRepository.cs
@@ -0,0 +1,14 @@
+﻿using _06Repository.Models;
+
+namespace _06Repository.DAL
+{
+    public interface IArticleRepository : IDisposable
+    {
+        IEnumerable<Article> GetAll();
+        Article Get(int ArticleId);
+        void Create(Article article);
+        void Update(Article article);
+        void Delete(int ArticleId);
+        void Save();
+    }
+}
